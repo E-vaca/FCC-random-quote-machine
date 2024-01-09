7b478c32fc0a157bd040ca566a894b86327848b0
vaxtale2023 <122852786+vaxtale2023@users.noreply.github.com>
2024-01-09 00:00:55 -0500

    Update App.tsx
    removed the right quote

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import quotes from "./assets/quotes.json"
-import {FaTwitter, FaQuoteLeft, FaQuoteRight} from "react-icons/fa"
+import {FaTwitter, FaQuoteLeft} from "react-icons/fa"
 import './App.css'
 
 interface Quote {
